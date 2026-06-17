f77022f2dfd2003ea24e73b2a0e782ef87431e33
hsuan <heidichen0528@gmail.com>
2026-06-17 22:57:59 +0800

    Update info.js

--- articles/2026/test/info.js
+++ articles/2026/test/info.js
@@ -1,6 +1,6 @@
 const postInfo = {
-    title: "我我我我",
-    category: "Coding", // 直接寫在這裡
-    tags: ["Coding", "Blog"],
+    title: "測試文",
+    category: "Life", // 直接寫在這裡
+    tags: ["test", "Blog"],
     date: "2026-06-17"
 };
