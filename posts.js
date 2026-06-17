3deab999b95a080421a039a7ba79730befbbb293
hsuan <heidichen0528@gmail.com>
2026-06-17 20:33:49 +0800

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,17 @@
+const categorySettings = [
+    { name: "Coding", limit: 3 },
+    { name: "Life", limit: 2 }
+];
+
+
+
+const posts = [
+{ 
+	title: "EGOI 2026 Contest", 
+	date: "2026-06-02", 
+	category: "Coding", 
+	tags: ["Competitive Programming"], 
+	link: "posts/egoi-contest.html" 
+}
+
+];
